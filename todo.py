b5b70d5f013d1c489ebdc1e9a2aa9c25aedae081
João Nunes <contato@nunes.sh>
2023-08-26 02:43:10 -0300

    arrumado crash ao tentar remover sem itens na lista

--- todo.py
+++ todo.py
@@ -2,8 +2,8 @@ import os
 
 todolist = ["arrumar a casa", "ajeitar site do canil"]
 
-def userUi():
-    os.system("clear") or None
+def userCli():
+    os.system("cls") or None
     i = 0
     if len(todolist) == 0:
         print("Nenhuma tarefa no momento.")
@@ -20,12 +20,15 @@ def userInput():
         case 1:
             addToList = input("Digite o nome da tarefa\n>> ")
             todolist.append(addToList)
-            userUi()
+            userCli()
         case 2:
-            removeFromList = int(input("Digite o número da tarefa a ser excluida.\n>> "))
-            todolist.pop(removeFromList-1)
-            userUi()
+            if len(todolist) == 0:
+                userCli()
+            else:
+                removeFromList = int(input("Digite o número da tarefa a ser excluida.\n>> "))
+                todolist.pop(removeFromList-1)
+                userCli()
         case 3:
             print("Até logo")
 
-userUi()
+userCli()
